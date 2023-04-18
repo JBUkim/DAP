5b0ef94b14a7401330179b2212658613edf79fe5
JBUkim <91812103@jmail.ac.kr>
2023-04-18 15:38:53 +0900

    2.0

--- common/forms.py
+++ common/forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
+    agree = forms.BooleanField(label="약관에 동의해주세요.")
 
     class Meta:
         model = User
